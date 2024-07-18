72fe2340e6b5186603b3eda37faf7280997b6acb
Jensone <hello@jensone.com>
2024-07-18 12:17:41 +0200

    🚀 ajouter(Categorie.php) : créer une nouvelle entité Categorie avec les champs id, nom, created_at, updated_at et les relations avec l'entité Recette 🚀 ajouter(CategorieRepository.php) : créer un nouveau repository pour l'entité Categorie pour effectuer des opérations de base de données liées à cette entité

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use App\Entity\Recette;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\Common\Collections\ArrayCollection;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[HasLifecycleCallbacks]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 80)]
+    private ?string $nom = null;
+
+    #[ORM\Column()]
+    private ?DateTimeImmutable $created_at = null;
+
+    #[ORM\Column()]
+    private ?DateTimeImmutable $updated_at = null;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->created_at = new \DateTimeImmutable();
+        $this->setUpdatedAtValue();
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdatedAtValue(): void
+    {
+        $this->updated_at = new \DateTimeImmutable();
+    }
+    
+    /**
+     * @var Collection<int, Recette>
+     */
+    #[ORM\OneToMany(targetEntity: Recette::class, mappedBy: 'categorie')]
+    private Collection $recettes;
+
+    public function __construct()
+    {
+        $this->recettes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Recette>
+     */
+    public function getRecettes(): Collection
+    {
+        return $this->recettes;
+    }
+
+    public function addRecette(Recette $recette): static
+    {
+        if (!$this->recettes->contains($recette)) {
+            $this->recettes->add($recette);
+            $recette->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecette(Recette $recette): static
+    {
+        if ($this->recettes->removeElement($recette)) {
+            // set the owning side to null (unless already changed)
+            if ($recette->getCategorie() === $this) {
+                $recette->setCategorie(null);
+            }
+        }
+
+        return $this;
+    }
+}
