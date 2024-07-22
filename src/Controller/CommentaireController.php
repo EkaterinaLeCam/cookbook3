156a4eefa7b76ed6fbea0b63a99dfbb3e3362ff2
Jensone <hello@jensone.com>
2024-07-22 13:32:21 +0200

    🔧 corriger(CommentaireController.php) : importer les classes manquantes 🔧 corriger(RecetteController.php) : importer la classe manquante PaginatorInterface 🔧 corriger(RecetteController.php) : ajouter la pagination pour afficher les recettes 🔧 corriger(RecetteController.php) : afficher les notes des recettes dans la vue individuelle

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Commentaire;
+use App\Form\CommentaireType;
 use App\Repository\CommentaireRepository;
+use App\Repository\RecetteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Egulias\EmailValidator\Parser\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,11 +25,49 @@ class CommentaireController extends AbstractController
             ),
         ]);
     }
-    #[Route('/commentaires{id}', name: 'app_commentaire_one', methods:['GET', 'POST'])]
-    public function commentaire_one(Commentaire $commentaire): Response
-    {
-        return $this->render('commentaire/commentaire_one.html.twig', [
-            'commentaire' => $commentaire,
-        ]);
+
+    #[Route('/commentaires/{id}', name: 'app_recette_commentaire_new', methods:['GET', 'POST'])]
+    public function nouveauCommentaire(
+        Commentaire $commentaire,
+        RecetteRepository $recetteRepository,
+        EntityManagerInterface $em,
+        Request $request,
+        ): Response {
+            $recette = $recetteRepository->findOneBy(['id' => $request->get('id')]);
+            $form = $this->createForm(CommentaireType::class);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // $ai = ModerationService::check($form->get('contenu')->getData());
+                $commentaire = new Commentaire();
+                $commentaire
+                    ->setContenu($form->get('contenu')->getData())
+                    ->setRecette($recette)
+                    ->setAuteur($this->getUser())
+                ;
+
+                // if ($ai = true) {
+                //     $commentaire->setStatut(true);
+                // }
+
+                $em->persist($commentaire);
+                $em->flush();
+
+                return $this->redirectToRoute('app_recette_one', ['slug' => $recette->getSlug()]);
+            }
+
+            return $this->render('commentaire/new.html.twig', [
+                'form' => $form,
+                'recette' => $recette
+            ]);
     }
+
+
+    // #[Route('/commentaires{id}', name: 'app_commentaire_one', methods:['GET', 'POST'])]
+    // public function commentaire_one(Commentaire $commentaire): Response
+    // {
+    //     return $this->render('commentaire/commentaire_one.html.twig', [
+    //         'commentaire' => $commentaire,
+    //     ]);
+    // }
 }
--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -4,27 +4,49 @@ namespace App\Controller;
 
 use App\Entity\Recette;
 use App\Repository\RecetteRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecetteController extends AbstractController
 {
+    // Afficher la liste des recettes
     #[Route('/recettes', name: 'app_recette', methods:['GET'])]
-    public function index(RecetteRepository $recetteRepository): Response
+    public function index(
+        RecetteRepository $recetteRepository, 
+        PaginatorInterface $paginator,
+        Request $request
+        ): Response
     {
+        $recettes = $paginator->paginate([
+            $recetteRepository->findBy([],['nom'=>'ASC']),
+            $request->query->getInt('page', 1),
+            16
+        ]);
+        
         return $this->render('recette/index.html.twig', [
-            'recettes' => $recetteRepository->findBy(
-                [],
-                ['nom'=>'ASC']
-            ),
+            'recettes' => $recetteRepository->findBy([],['nom'=>'ASC']),
         ]);
     }
-    #[Route('/recettes{id}', name: 'app_recette_one', methods:['GET'])]
-    public function recette(Recette $recette): Response
+
+    // Afficher une recette
+    #[Route('/recette/{slug}', name: 'app_recette_one', methods:['GET', 'POST'])]
+    public function recette(
+        RecetteRepository $recette,
+        string $slug
+        ): Response
     {
+        $selection = $recette->findOneBy(['slug' => $slug])->getNotes();
+        $notes = [];
+        foreach ($selection as  $value) {
+            array_push($notes, $value->getEtoile());
+        }
+    
         return $this->render('recette/recette_one.html.twig', [
-            'recette' => $recette,
+            'recette' => $recette->findOneBy(['slug' => $slug]),
+            'notes' => array_count_values($notes),
         ]);
     }
 }
