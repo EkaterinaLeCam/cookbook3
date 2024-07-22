6a5afe390ba59287382cd028db8167bd2bf03292
Jensone <hello@jensone.com>
2024-07-22 13:32:34 +0200

    🔧 corriger(security.yaml) : mettre à jour le chemin d'accès pour les commentaires ✨ fonctionnalité(CommentaireType.php) : ajouter un nouveau formulaire pour les commentaires ✨ fonctionnalité(new.html.twig) : ajouter un formulaire pour ajouter un commentaire sur une recette 🎨 mise en page(recette_one.html.twig) : améliorer l'affichage des commentaires et ajouter un lien pour ajouter un commentaire

---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use App\Entity\Recette;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenu', TextareaType::class, [
+                'label' => 'Écrivez votre commentaire',
+                'label_attr' => [
+                    'class' => 'mb-3 flex items-center mb-2 text-gray-600 text-sm font-medium'
+                ],
+                'attr' => [
+                    'class' => 'mb-3 rounded-md border border-gray-300 shadow-sm',
+                    'placeholder' => 'Écrivez votre commentaire',
+                    'rows' => 5,
+                    'col' => 12
+                ],
+            ])
+            ->add('recette', HiddenType::class, [
+                'mapped' => false,
+            ])
+            ->add('auteur', HiddenType::class, [
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
