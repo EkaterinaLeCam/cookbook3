15a004c8b04d9ef533cecde679eec3de19904526
Jensone <hello@jensone.com>
2024-07-25 11:50:55 +0200

    🔧 config(services.yaml) : ajouter le chemin des téléversements d'images 🚀 fonctionnalité(AccueilController.php) : ajouter un formulaire de contact 🆕 ajout(ContactType.php) : ajouter un formulaire de contact 🎨 mise à jour(contact.html.twig) : afficher le formulaire de contact et gérer les messages Flash

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,30 +6,65 @@ use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil', methods:['GET'])]
+    #[Route('/', name: 'app_accueil', methods: ['GET'])]
     public function index(RecetteRepository $recetteRepository): Response
     {
         return $this->render('page/accueil.html.twig', [
             'recettes' => $recetteRepository->findBy(
                 [],
-                ['id'=>'DESC'],10
-             
+                ['id' => 'DESC'],
+                10
+
             ),
         ]);
     }
-    #[Route('/conditions-generales', name: 'app_conditions_generales', methods:['GET'])]
+    #[Route('/conditions-generales', name: 'app_conditions_generales', methods: ['GET'])]
     public function conditions_generale(): Response
     {
-        return $this->render('page/conditions-generales.html.twig' );
-       
+        return $this->render('page/conditions-generales.html.twig');
     }
-    #[Route('/contact', name: 'app_contact', methods:['GET', 'POST'])]
-    public function contact(): Response
+
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function contact(Request $request): Response
     {
-        return $this->render('page/contact.html.twig' );
-       
+        // Importation du formulaire
+        $form = $this->createForm(ContactType::class);
+
+        // Saisir les données du formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // dd($form->getData());
+
+            // Traitement de l'upload
+            $file = $form->get('curriculum')->getData();
+            if ($form->get('curriculum')->getData()) {
+                $cvFile = $form->get('curriculum')->getData();
+                $originalFilename = pathinfo($cvFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $cvFile->guessExtension();
+
+                try {
+                    $cvFile->move(
+                        $this->getParameter('uploads'),
+                        $newFilename
+                    );
+                    $this->addFlash('success', 'Votre CV a bien été envoyé');
+                    return $this->redirectToRoute('app_contact');
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de votre CV');
+                }
+            }
+        }
+
+        return $this->render('page/contact.html.twig', [
+            'contactForm' => $form, // Transfert du formulaire au template
+        ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Votre nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+            ])
+            ->add('curriculum', FileType::class, [
+                'label' => 'Envoyer un CV',
+                'required' => false,
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => '',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre message',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Votre message doit contenir au moins {{ limit }} caractères',
+                        'max' => 1000,
+                        'maxMessage' => 'Votre message ne peut pas dépasser {{ limit }} caractères'
+                    ])
+                ],
+            ])
+            ->add('rgpd', CheckboxType::class, [
+                'label' => '',
+                'required' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'w-full h-12 text-white text-base font-semibold leading-6 rounded-full transition-all duration-700 hover:bg-indigo-800 bg-indigo-600 shadow-sm'
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
