b4cfc95ed39e1b8cd9b68c321b411c4d0dc79be4
Jensone <hello@jensone.com>
2024-07-25 12:26:34 +0200

    🔄 mise à jour(translation.yaml) : changer la langue par défaut en français (fr) 🔧 refactor(IngredientCrudController.php) : ajouter des champs pour le CRUD des ingrédients 🔧 refactor(RecetteCrudController.php) : ajouter des champs pour le CRUD des recettes et configurer les actions

--- src/Controller/Admin/IngredientCrudController.php
+++ src/Controller/Admin/IngredientCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +17,21 @@ class IngredientCrudController extends AbstractCrudController
         return Ingredient::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnDetail(),
+            TextField::new('nom'),
+
+            FormField::addPanel('Avec une image c\'est mieux !')
+                ->setIcon('image')
+                ->setHelp('Saisissez l\'image de la recette'),
+            ImageField::new('image')
+                ->setUploadedFileNamePattern('[contenthash].[extension]')
+                ->setBasePath('uploads/ingredients')
+                ->setUploadDir('public/uploads/ingredients')
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/RecetteCrudController.php
+++ src/Controller/Admin/RecetteCrudController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recette;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +20,30 @@ class RecetteCrudController extends AbstractCrudController
         return Recette::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+{
+    return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+    ;
+}
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnDetail(),
+            FormField::addPanel('Quelle est le nom de la recette ?')
+                ->setIcon('cube')
+                ->setHelp('Saisissez le nom de la recette'),
+            TextField::new('nom'),
+
+            FormField::addPanel('Avec une image c\'est mieux !')
+                ->setIcon('image')
+                ->setHelp('Saisissez l\'image de la recette'),
+            ImageField::new('image')
+                ->setUploadedFileNamePattern('[contenthash].[extension]')
+                ->setBasePath('uploads/recettes')
+                ->setUploadDir('public/uploads/recettes')
+            // TextEditorField::new('description'),
         ];
     }
-    */
 }
