d1dce9f36317a58511adf90002a27d56c4bd857b
katia <katia.lecam@gmail.com>
2024-08-08 14:45:11 +0200

    Search Data class

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Model\SearchData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,6 +23,7 @@ class SearchType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            'data_class'=> SearchData::class,
             'method'=> 'GET',
             //CSRF : principes, impacts et bonnes pratiques sécurité
             'csrf_protection'=>false
