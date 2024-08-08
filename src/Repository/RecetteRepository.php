f054257644cd6f14c4c19757b9e065cdf80ee5e8
katia <katia.lecam@gmail.com>
2024-08-08 16:50:50 +0200

    recetteReposytory le resultat de search bar

--- src/Repository/RecetteRepository.php
+++ src/Repository/RecetteRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Recette;
+use App\Model\SearchData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,7 +17,21 @@ class RecetteRepository extends ServiceEntityRepository
         parent::__construct($registry, Recette::class);
     }
 
+    public function findBySearch(SearchData $searchData)
+    {
+        $data= $this;
+        if(!empty($searchData->q))
+        {
+            $data=$data
+            ->setParameter('q', "%{$searchData->q}%");
+        }
+        $data=$data
+        ->getQuery()
+        ->getResult();
 
+        $recettes = $this->paginatorInterface->paginate($data, $searchData->page, 9);
+        return $recettes;
+    }
 
 
 }
