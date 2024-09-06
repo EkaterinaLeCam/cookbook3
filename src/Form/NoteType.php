a6ebf86aa17e53d82b4d84259a6578df9bc12c68
katia <katia.lecam@gmail.com>
2024-09-06 18:26:21 +0200

    noter la recette

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -8,9 +8,10 @@ use App\Entity\Utilisateur;
 use DateTimeInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,17 +20,22 @@ class NoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('etoile', IntegerType::class, [
-            'label' => 'Votre note',
-            'required' => true,
-            'attr' => [
-                'min' => 1,
-                'max' => 5,
-                'placeholder' => 'Choisissez une note (1-5)',
-            ],
-        ])
-        ->add('submit', SubmitType::class);
-        
+            ->add('etoile', IntegerType::class, [
+                'label' => 'votre note',
+                'required' => true,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 5,
+                    'placeholder' => 'Choisissez une note (1-5)',
+                ],
+
+            ])
+            ->add('recette', HiddenType::class, [
+                'mapped' => false,
+            ])
+            ->add('auteur', HiddenType::class, [
+                'mapped' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -39,5 +45,3 @@ class NoteType extends AbstractType
         ]);
     }
 }
-
-
